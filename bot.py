4d7aaedff052de443d7d5b8919fd38423e624fe3
RyoCoder <89810037+RyoCoder@users.noreply.github.com>
2022-01-28 22:36:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,3 +81,86 @@ async def unmute(client, cb):
 
 
 tagcheck.run()
+from pyrogram import filters, Client
+import logging
+import os
+from pyrogram.types import (
+   ChatPermissions,
+   InlineKeyboardButton,
+   InlineKeyboardMarkup
+)
+
+logging.basicConfig(level=logging.INFO)
+
+API_ID = int(os.environ.get("API_ID", 6))
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+TOKEN = os.environ.get("TOKEN", None)
+TAG = os.environ.get("TAG", None)
+OWNER_ID = int(os.environ.get("OWNER_ID", 1704673514))
+
+
+tagcheck = Client(
+   "tagcheck",
+   bot_token=TOKEN,
+   api_id=API_ID,
+   api_hash=API_HASH
+)
+
+
+async def is_admin(message):
+    user = await tagcheck.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status in ("administrator", "creator"):
+      return True
+    return False
+
+@tagcheck.on_message(filters.command("start") & filters.user(OWNER_ID))
+async def start(_, message):
+   await message.reply("I am Alive.")
+
+@tagcheck.on_message(filters.group)
+async def tag_check(_, message):
+    if await is_admin(message):
+       return
+    user = message.from_user.id
+    if TAG not in message.from_user.first_name:
+       try:
+           await tagcheck.restrict_chat_member(
+             message.chat.id,
+             user,
+             ChatPermissions(),
+           )
+       except BaseException as be:
+           await message.reply(f"**Error:**\n`{be}`")
+           return
+       text = f"""
+**Nào {message.from_user.mention}**
+Vui lòng thêm hashtag của nhóm vào tên của bạn để
+có thể trò chuyện lại trong nhóm.
+
+**Hashtag:** `{TAG}`
+**Ghi chú:** __Bấm vào nút bên dưới cho
+mở chat cho bản thân!__
+"""
+       await message.reply(
+        text,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Mở chat", callback_data=f"unmute_{user}")]
+           ]
+         )
+       )
+
+@tagcheck.on_callback_query(filters.regex("unmute_(.*)"))
+async def unmute(client, cb):
+    user = int(cb.matches[0].group(1))
+    if cb.from_user.id != user:
+      await cb.answer("Nút này không dành cho bạn!", show_alert=True)
+      return
+    if TAG in cb.from_user.first_name:
+      await tagcheck.unban_chat_member(cb.message.chat.id, user)
+      await cb.answer("Mở chat thành công!")
+      await message.delete()
+      return
+    await cb.answer("Vui lòng thêm thẻ vào tên của bạn!", show_alert=True)
+
+
+tagcheck.run()
